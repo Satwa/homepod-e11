92c37a96ddef340a5e625028cf7bd2f6fa8b8875
Joshua <satwa.fr@gmail.com>
2019-02-19 10:01:39 +0100

    CTA style

--- assets/scripts/trySiri.min.js
+++ assets/scripts/trySiri.min.js
@@ -1,28 +1,31 @@
 const siriBlock = document.querySelector(".onSiri")
+let isAlreadyPlaying = false
+
+let playAudio = (name) => {
+    console.log(window.location.origin + "/assets/audio/" + name + ".mp3")
+    let audio = new Audio(window.location.origin + "/assets/audio/" + name + ".mp3")
+    // audio.type = 'audio/mp3'
+
+    let play = audio.play()
+    
+
+    play.then(() => {
+        siriBlock.style.opacity = 1
+        annyang.pause()
+    }).catch((err) => console.log("Error happened:", err))
+    
+    audio.addEventListener("ended", () => {
+        isAlreadyPlaying = false
+        siriBlock.style.opacity = 0
+        annyang.resume()
+    })
+}
+
 if(annyang){
     let tellMeAbout = (name) => {
         siriBlock.style.opacity = 1
         console.log(name)
         // Dans tous les cas c'est G.
-    }
-
-    let playAudio = (name) => {
-        console.log(window.location.origin + "/assets/audio/" + name + ".mp3")
-        let audio = new Audio(window.location.origin + "/assets/audio/" + name + ".mp3")
-        // audio.type = 'audio/mp3'
-
-        let play = audio.play()
-        
-
-        play.then(() => {
-            siriBlock.style.opacity = 1
-            annyang.pause()
-        }).catch((err) => console.log("Error happened:", err))
-        
-        audio.addEventListener("ended", () => {
-            siriBlock.style.opacity = 0
-            annyang.resume()
-        })
     }
 
     let commands = {
@@ -54,6 +57,16 @@ if(annyang){
 
     annyang.start({ autoRestart: true, continuous: false })
 }else{
-    alert("DEBUG: La reconnaissance vocale n'est pas supportée")
+    const questions = document.querySelectorAll(".siriGradient")
+    questions.forEach((question) => {
+        question.addEventListener("click", function(e) {
+            // Disable * questions
+            // Play audio
+            if(!isAlreadyPlaying){
+                playAudio("watusay")
+            }
+        })
+    })
+    // alert("DEBUG: La reconnaissance vocale n'est pas supportée")
     // Ajouter un clickListener sur les phrases
 }
