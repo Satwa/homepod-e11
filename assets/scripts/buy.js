f096c97ec7dcad65ec39d6e36408dd12b898a8eb
Joshua <satwa.fr@gmail.com>
2019-02-19 17:16:46 +0100

    Buy: Fin d'intégration // WIP: Responsive

--- assets/scripts/buy.js
+++ assets/scripts/buy.js
@@ -1,14 +1,23 @@
 const homepod = document.querySelectorAll(".homepod")
 const colors = document.querySelectorAll(".colorSelection")
 
+let pricePanel = document.querySelector("#price")
+let price = 349
+
 document.querySelector(".addCarePlan").addEventListener("click", function(e){
     let selected = this.getAttribute("data-selected")
     if(selected == "false"){
         this.classList.add("active")
         this.setAttribute("data-selected", "true")
+        price += 45
+        pricePanel.innerHTML = price + ",00€"
+        document.querySelector("#appleCare").innerHTML = "Avec"
     }else{
         this.classList.remove("active")
         this.setAttribute("data-selected", "false")
+        price -= 45
+        pricePanel.innerHTML = price + ",00€"
+        document.querySelector("#appleCare").innerHTML = "Sans"
     }
 })
 
@@ -28,6 +37,8 @@ colors.forEach((color) => {
             if(elm.id == selected){
                 color.classList.add("active")
                 elm.classList.add("active")
+                
+                document.querySelector("#colortype").innerHTML = this.innerHTML
             }else{
                 elm.classList.add("back")
             }
