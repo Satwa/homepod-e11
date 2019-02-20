daa3c16cda0ddf8cb3a743e25383f50fdd0e72b2
Joshua <satwa.fr@gmail.com>
2019-02-20 10:42:33 +0100

    Discover & Buy

--- assets/scripts/trySiri.js
+++ assets/scripts/trySiri.js
@@ -31,6 +31,10 @@ if(annyang){
         playAudio("weather")
     }
 
+    let tellPodcast = (name) => { // In case we want to build a real assistant
+        playAudio("podcast")
+    }
+
     let commands = {
         'dis siri': function(){
             playAudio("question")
@@ -49,6 +53,9 @@ if(annyang){
         '(dis siri) reprends ma musique': function(){
             playAudio("music")
         },
+        '(dis siri) joue le dernier épisode de *name': tellPodcast, 
+        '(dis siri) reprend l\'épisode de *name': tellPodcast, 
+        '(dis siri) reprend la lecture de *name': tellPodcast, 
         '(dis siri) quelle est la météo (du jour)': tellWeather,
         '(dis siri) quelle est la météo (d\'aujourd\'hui)': tellWeather,
         '(dis siri) quelle est la météo (à *city)': tellWeather,
