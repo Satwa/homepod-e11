d753d4760c63a66b0d85c0424de2c2a6eea4c862
RomainPct <romain.penchenat@gmail.com>
2019-02-21 10:43:08 +0100

    - Footer responsive

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,8 +1,25 @@
 // Hamburger menu
-const menu = document.querySelector("#rightMenu ul")
+const menu = document.querySelector("#rightMenu ul"),
+      footerLists = document.querySelectorAll('.footerList')
 let previousWidth = 0
 
 document.querySelector("#mobileMenu").addEventListener("click", function(){00
   this.classList.toggle("is-active")
   menu.classList.toggle("is-active")
 })
+
+for (let i = 0; i < footerLists.length; i++) {
+  footerLists[i].addEventListener('click',function() {
+    if (document.documentElement.clientWidth < 601) {
+      toggleFooterList(i)
+    }
+  })
+}
+
+function toggleFooterList(i) {
+  let opened = document.querySelector(".footerList.opened")
+  if (opened != null && opened != footerLists[i]) {
+    opened.classList.remove("opened")
+  }
+  footerLists[i].classList.toggle("opened")
+}
